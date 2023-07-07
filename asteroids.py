63b4c7f79bf84ac927ed4fc9115cd3e5ebbefea5
Jonatan Jansson <jonatan.jansson@duva.se>
2023-07-07 17:38:16 +0200

    fixa lite också

--- asteroids.py
+++ asteroids.py
@@ -49,6 +49,27 @@ pygame.font.init()
 custom_font = pygame.font.Font(None, 24)
 game_started = False
 
+# Health bar properties
+health = 100
+healthbar_width = 200
+healthbar_height = 20
+healthbar_border_width = 2
+healthbar_border_color = (255, 255, 255)
+healthbar_inner_color = (0, 255, 0)
+healthbar_x = WIDTH - healthbar_width - 10
+healthbar_y = 10
+
+# High score properties
+high_score = 0
+score = 0
+score_x = 10
+score_y = 10
+
+# Game over properties
+game_over = False
+game_over_text = custom_font.render("Game Over", True, (255, 0, 0))
+game_over_rect = game_over_text.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+
 # Main loop
 running = True
 frame_count = 0
@@ -96,12 +117,21 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            # Shooting bullets
+            if event.key == pygame.K_LEFT:
+                ship_angle += turn_speed
+            if event.key == pygame.K_RIGHT:
+                ship_angle -= turn_speed
+            if event.key == pygame.K_UP:
+                ship_x += math.cos(math.radians(ship_angle)) * speed
+                ship_y -= math.sin(math.radians(ship_angle)) * speed
+                moving = True
             if event.key == pygame.K_SPACE and frame_count >= bullet_timer:
                 bullet_x = ship_x + math.cos(math.radians(ship_angle)) * (ship_img.get_width() / 2)
                 bullet_y = ship_y - math.sin(math.radians(ship_angle)) * (ship_img.get_height() / 2)
                 bullets.append((bullet_x, bullet_y, ship_angle))
                 bullet_timer = frame_count + bullet_visibility_toggle
+
+
 
     # Screen wrapping for ship
     ship_x = (ship_x + WIDTH) % WIDTH
@@ -144,6 +174,9 @@ while running:
         scaled_asteroid = pygame.transform.scale(asteroid_img, (int(asteroid_img.get_width() * scale), int(asteroid_img.get_height() * scale)))
         win.blit(scaled_asteroid, (x, y))
 
+    # Create a list to store bullets that need to be removed
+    bullets_to_remove = []
+
     # Draw and update bullets
     for i, (x, y, angle) in enumerate(bullets):
         dx = math.cos(math.radians(angle)) * bullet_speed
@@ -157,35 +190,54 @@ while running:
 
         # Remove bullets that leave the screen
         if x < 0 or x > WIDTH or y < 0 or y > HEIGHT:
-            bullets.pop(i)
-            continue
-
+            bullets_to_remove.append(i)
         # Update the bullet's position
         bullets[i] = (x, y, angle)
 
         # Draw the bullet
         pygame.draw.circle(win, bullet_color, (int(x), int(y)), bullet_radius)
 
+
         # Collision detection between bullets and asteroids
-    for b_index, (bx, by, b_angle) in enumerate(bullets):
         for a_index, (ax, ay, adx, ady, ascale) in enumerate(asteroids):
             scaled_asteroid_width = asteroid_img.get_width() * ascale
             scaled_asteroid_height = asteroid_img.get_height() * ascale
-            if ax < bx < ax + scaled_asteroid_width and ay < by < ay + scaled_asteroid_height:
-                # Remove the bullet
-                bullets.pop(b_index)
-                
-                # Split the asteroid into two smaller ones
-                new_scale = ascale * 0.5
-                if new_scale > 0.1:  # Only split if the new asteroids won't be too small
-                    asteroids.append((ax, ay, adx * 0.7, ady + 1, new_scale))  # To the "west"
-                    asteroids.append((ax, ay, adx * 0.7, ady - 1, new_scale))  # To the "east"
-                
-                # Remove the original asteroid
-                asteroids.pop(a_index)
-                break  # Exit the inner loop since the bullet is already removed
+            for b_index, (bx, by, angle) in enumerate(bullets):
+                if ax < bx < ax + scaled_asteroid_width and ay < by < ay + scaled_asteroid_height:
+                    # Split the asteroid into two smaller ones
+                    new_scale = ascale * 0.5
+                    if new_scale > 0.1:  # Only split if the new asteroids won't be too small
+                        asteroids.append((ax, ay, adx * 0.7, ady + 1, new_scale))  # To the "west"
+                        asteroids.append((ax, ay, adx * 0.7, ady - 1, new_scale))  # To the "east"
+
+                    # Remove the original asteroid
+                    asteroids.pop(a_index)
+
+                    # Remove the bullet
+                    bullets.pop(b_index)
+
+                    # Update the score
+                    score += 1
+                    break
+            else:
+                continue
+            break
 
 
+            # Remove the original asteroid
+            asteroids.pop(a_index)
+
+            # Remove the bullet
+            bullets.pop(b_index)
+
+            # Update the score
+            score += 1
+            break
+
+
+    # Remove bullets that need to be removed
+    for index in bullets_to_remove:
+        bullets.pop(index)
 
     # Draw teal shimmer around border
     s = pygame.Surface((WIDTH, HEIGHT), pygame.SRCALPHA)
@@ -195,6 +247,34 @@ while running:
     pygame.draw.rect(s, shimmer_color, (WIDTH - shimmer_thickness, 0, shimmer_thickness, HEIGHT))
     win.blit(s, (0, 0))
 
+    # Draw health bar
+    pygame.draw.rect(win, healthbar_border_color, (healthbar_x, healthbar_y, healthbar_width, healthbar_height), healthbar_border_width)
+    pygame.draw.rect(win, healthbar_inner_color, (healthbar_x, healthbar_y, healthbar_width * health / 100, healthbar_height))
+
+    # Draw high score
+    high_score_text = custom_font.render("High Score: " + str(high_score), True, (255, 255, 255))
+    win.blit(high_score_text, (score_x, score_y))
+
+    # Draw score
+    score_text = custom_font.render("Score: " + str(score), True, (255, 255, 255))
+    win.blit(score_text, (score_x, score_y + 30))
+
+    # Check game over condition
+    if health <= 0:
+        game_over = True
+
+    # Game over logic
+    if game_over:
+        win.blit(game_over_text, game_over_rect.topleft)
+
+        # Reset game if 'P' is pressed
+        if keys[pygame.K_p]:
+            game_over = False
+            score = 0
+            health = 100
+            bullets.clear()
+            asteroids.clear()
+
     # Update the screen
     pygame.display.flip()
     pygame.time.Clock().tick(60)
@@ -203,4 +283,4 @@ while running:
     frame_count += 1
 
 # Quit pygame
-pygame.quit() # End pygame    
+pygame.quit()  # End pygame
